7e91a11bae76d9115c3c9befc14d53d98c77061e
Kent <xq-310@163.com>
2026-09-16 11:20:44 +0800

    Publish tokenization and blockchain essay with illustration

--- pages/work/_meta.js
+++ pages/work/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: "栏目说明",
+  "chain": "当金融开始 Token 化，我开始重新思考公链和联盟链",
   "stablefx-sandbox-testing-analysis": "测试了一遍 Circle StableFX：关于链上外汇、技术架构和商业模式的一些思考",
   "circle-transition-revenue-structure": "Circle的盛世隐忧：产品已经转型，收入结构还没有",
   "circle-partnership-and-stablecoin-startups": "当我们与 Circle 谈合作时，稳定币创业公司还剩下什么？"
